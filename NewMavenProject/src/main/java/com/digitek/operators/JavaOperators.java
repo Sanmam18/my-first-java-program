73b0170637ef6d578e075f96882ef36ee5ed1391
Sanmam18 <sanmam18@gmail.com>
2015-10-18 15:07:03 -0700

    First Operator commit
    First Operator commit

---
+++ NewMavenProject/src/main/java/com/digitek/operators/JavaOperators.java
@@ -0,0 +1,27 @@
+package com.digitek.operators;
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+		javaOperatorsDemo();
+
+	}
+	
+	public static void javaOperatorsDemo(){
+		
+		int no1 = 21;
+		int no2 = 34;
+		 
+		int result = no1 + no2;
+		System.out.println("Result : "+result);
+		
+		String s1 =  "Hello";
+		String s2 = "World";
+		
+		//String concate= s1 + s2;
+		
+		System.out.println("Concatetion  : "+s1+" "+s2);
+		
+	}
+
+}
